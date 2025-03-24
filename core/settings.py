8d5243a21c52782aa1608de1df886b211442ff18
bex2009ake <moneybusinessdollar@gmail.com>
2025-03-24 11:55:08 +0500

    Static

--- core/settings.py
+++ core/settings.py
@@ -133,12 +133,12 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 
-STATIC_ROOT = os.path.join(STATIC_URL, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 MEDIA_URL = "/media/"
 
-MEDIA_ROOT = os.path.join(STATIC_URL, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
